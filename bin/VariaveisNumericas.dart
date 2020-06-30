f216f78579ab6e55775b4caee94c46676983f2cd
Lucas-Bispo <lukaobispo@gmail.com>
2020-06-30 08:24:14 -0300

    Tipos numericos em dart

---
+++ bin/VariaveisNumericas.dart
@@ -0,0 +1,15 @@
+main() {
+  //Int Double -> num -> qualquer tipo de número
+  num age =24;
+  
+  //inteiro
+  int people = 5;
+
+  // Decimal 
+  double decimal = 20.02;
+
+  //Conversão de tipo
+  int test = int.parse('12');
+
+  print(age);
+}
