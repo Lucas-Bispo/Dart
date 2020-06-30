a9c13a425c9e4abdba22b73a567db4c4a77aea28
Lucas-Bispo <lukaobispo@gmail.com>
2020-06-30 08:00:58 -0300

    comentando a main

--- bin/Dart.dart
+++ bin/Dart.dart
@@ -1,5 +1,9 @@
+/*Esse import, importa um arquivo 
+e carrega ele como uma variável */
 import 'package:Dart/Dart.dart' as Dart;
 
+//Esse é o ponto de partida da aplicação
 void main(List<String> arguments) {
+  //Afunção que imprime algo na tela
   print('Hello world: ${Dart.calculate()}!');
 }
