f83656763d60566a9d57dc9e2e609d83854c54fe
Lucas-Bispo <lukaobispo@gmail.com>
2020-06-30 08:33:08 -0300

    Funções para variáveis numéricas

---
+++ lib/FuncoesParaVariaveisNumericas.dart
@@ -0,0 +1,15 @@
+main() {
+  num  numero = 1.15;
+  
+  print(numero.round());
+  print(numero.compareTo(20));
+  print(numero.toInt());
+  print(10.toDouble());
+  print(numero.toString()is String);
+  print(numero.toStringAsFixed(1));
+  print(numero.isInfinite);
+  print((numero/0).isInfinite);
+  print(numero.truncate());
+  print(numero.floor());
+
+}
