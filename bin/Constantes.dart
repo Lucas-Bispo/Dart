a3a1daaeb6816280ea4bee4215c1c7b1aa7e90f8
Lucas-Bispo <lukaobispo@gmail.com>
2020-06-30 16:40:42 -0300

    constante

---
+++ bin/Constantes.dart
@@ -0,0 +1,11 @@
+main() {
+  // cria uma constante e, tempo de compilação
+  const pi = 3.14;
+  print('Ovalor de pi é $pi');
+  // Cria uma constante em tempo de execução
+  final lucas = new Pessoa();
+  print(lucas);
+}
+class Pessoa{
+
+}
