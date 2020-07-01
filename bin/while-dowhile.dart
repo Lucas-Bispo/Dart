a0651e72683ff9e5eed41d78dffd9fcb3fff32b7
Lucas-Bispo <lukaobispo@gmail.com>
2020-07-01 19:06:26 -0300

    do while

---
+++ bin/while-dowhile.dart
@@ -0,0 +1,17 @@
+main() {
+
+//  int numero = 0;
+//
+//  while(numero <= 10) {
+//    print('While: $numero');
+//    numero++;
+//  }
+
+  int contagem = 0;
+
+  do {
+    print("doWhile: $contagem");
+    contagem++;
+  } while(contagem <= 7);
+
+}
