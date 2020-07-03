765b6835063d13aa94a206e603126409bda99f94
Lucas-Bispo <lukaobispo@gmail.com>
2020-07-03 10:04:06 -0300

    TimetsCallbacks

---
+++ bin/TimetsCallbacks.dart
@@ -0,0 +1,27 @@
+import 'dart:developer';
+
+import 'package:test/test.dart';
+
+import 'dart:async';
+
+int counter = 0;
+main() {
+  Duration duration = Duration(seconds: 2);
+  Timer.periodic(duration, timeout);
+
+  print('Iniciado ${getTime()}');
+}
+
+void timeout(Timer timer){
+  print('Espera: ${getTime()}');
+
+  counter++;
+  if(counter >= 5){
+    timer.cancel();
+  }
+}
+
+String getTime(){
+  DateTime dt = DateTime.now();
+  return dt.toString();
+}
