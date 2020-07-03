681ee57fb3e495f16a262ba34a0eea60e6118818
Lucas-Bispo <lukaobispo@gmail.com>
2020-07-03 18:22:04 -0300

    Rivisar algoritimo

---
+++ bin/zip.dart
@@ -0,0 +1,51 @@
+import 'dart:io';
+import 'package:archive/archive.dart';
+import 'package:path/path.dart';
+
+main() {
+  List<String> files = List<String>();
+
+  Directory.current.listSync().forEach((FileSystemEntity fse) { 
+    if(fse.statSync().type == FileSystemEntityType.file)
+    files.add(fse.path);
+
+  });
+
+  String zipfile = '${Directory.current.path}/comprimido.zip';
+
+  zip(files, zipfile);
+
+  //unzip(zipfile, '${Directory.current.path}');//descomprimindo 
+
+}
+
+void zip(List<String> files, String file){
+  Archive archive = Archive();
+
+  files.forEach((String path) { 
+
+    File file = File(path);
+
+    ArchiveFile archiveFile = ArchiveFile(p.basename(path), file.lengthSync(), file.readAsBytesSync());
+    archive.addFile(archiveFile);
+  });
+
+  ZipEncoder encoder = ZipEncoder();
+  File f = File(file);
+  f.writeAsBytesSync(encoder.encode(archive));
+  print('Comprimido com sucesso!');
+}
+
+void unzip(String zip, String path){
+  File file = File(zip);
+  Archive archive = ZipDecoder().decodeBytes(file.readAsBytesSync());
+
+  archive.forEach((ArchiveFile archiveFile) { 
+    File file = File(path + '/' + archiveFile.name);
+    file.createSync(recursive: true);
+    file.writeAsBytes(archivefile.content);
+  });
+
+  print('Descomprimido');
+
+}
