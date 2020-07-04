4e99f5577eb79e467dc79bd247123e8c43fabacc
Lucas-Bispo <lukaobispo@gmail.com>
2020-07-04 08:22:06 -0300

    HTTPPost

---
+++ bin/HTTPPost.dart
@@ -0,0 +1,8 @@
+import 'package:http/http.dart' as http;
+
+main() async{
+  var url = 'http://dartlang.otg/';
+  var response = await http.post(url, body: 'nome=Leo&idade23');
+  print('O servidor respondeu com o código de status: ${response.statusCode}');
+  print('O corpo de requisição é:\n\n ${response.body}');
+}
